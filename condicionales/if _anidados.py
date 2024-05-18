e3f16a6431768c1e9f44e7eeb805ee6439fb9b5d
jesusgabriel9696@gmail.com <jesusgabriel9696@gmail.com>
2024-05-18 15:48:41 -0400

    Condicionales

---
+++ condicionales/if _anidados.py
@@ -0,0 +1,16 @@
+
+dia= "Lunes"
+
+estas_enfermo= True
+
+if dia == "Lunes":
+    print("Hoy voy al colegio")
+
+    if estas_enfermo==False:
+        print("No estoy enfermo, voy a poder ir al colegio")
+    
+    else:
+        print("pero no iré al colegio porque estoy enfermo")
+
+else: 
+    print("Hoy no es lunes, no hay clases")
