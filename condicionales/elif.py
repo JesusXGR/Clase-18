df67f0cee09fe551a901790f001991a9abe5eaa3
jesusgabriel9696@gmail.com <jesusgabriel9696@gmail.com>
2024-05-18 15:52:50 -0400

    modificicaciones que no se agregaron antes

---
+++ condicionales/elif.py
@@ -0,0 +1,15 @@
+dia = 2
+
+if dia == 1:
+    print("Hoy es lunes")
+
+elif dia == 2:
+    print("Es martes")
+
+elif dia == 3:
+    print("Es miércoles")
+
+else:
+    print("No hay trabajo")
+
+    
